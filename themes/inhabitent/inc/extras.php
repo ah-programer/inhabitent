dbd74c800f2f5b02707cd6b24191f280c61423e3
Ahlam <ahlam@Ahlams-MacBook-Air.local>
2016-08-12 12:06:10 -0700

    first commit

---
+++ themes/inhabitent/inc/extras.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function red_starter_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+
+
+
+function inhabitent_remove_submenus() {
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
+
+function my_login_logo(){?>
+	style type="text/css">
+	#login h1 a, login h1 a{
+ background-image:url(<?php echo get_template_directory_uri(); ?>/images/
+ inhabitent-logo-text
